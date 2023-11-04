299701b75f35fcffb84a92473beda273a012c8da
diogohal <dha21@inf.ufpr.br>
2023-11-04 00:05:22 -0300

    teste

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'diogohal', // Usually your GitHub org/user name.
-  projectName: 'diogohal', // Usually your repo name.
+  projectName: 'web-portfolio-docusaurus', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
